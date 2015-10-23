0c2ff8f229053b3a0c987f13861b746e9cbb1fd6
elianaovita <ovitaeliana@gmail.com>
2015-10-22 21:50:37 -0700

    QUIS

---
+++ src/newpackage/lingkaran.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author deny setiawan
+ */
+public class lingkaran {
+    public double hasil;
+    public double lingkaran (double r){
+       hasil = 22*r*r/7;
+       return hasil;
+    }
+    
+}
